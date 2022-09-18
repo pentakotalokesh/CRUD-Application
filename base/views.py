7b40d769cd87f14244ee5158168e8d399f019253
pentakotalokesh <pentakotalokesh67@gmail.com>
2022-09-18 15:03:50 +0530

    Errors handled

--- base/views.py
+++ base/views.py
@@ -41,13 +41,16 @@ def register(request):
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
-        if not (User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists()):
-            User.objects.create_user(username, email, password)
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect('home')
+        if username != '' or email != '' or password != '':
+            if not (User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists()):
+                User.objects.create_user(username, email, password)
+                user = authenticate(username=username, password=password)
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request,'Looks like a username with that email or password already exists')
         else:
-            messages.error('Looks like a username with that email or password already exists')
+            messages.error(request,'All fields required')
     return render(request, 'base/Create.html')
 
 
